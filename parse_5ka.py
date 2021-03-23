ac28ec4e710686b71dd51c720deda8cd8247d13e
Крамчанинов Денис <kramchaninovd@mail.ru>
2021-03-24 00:30:20 +0700

    практика урока 1

--- parse_5ka.py
+++ parse_5ka.py
@@ -26,10 +26,10 @@ class Parse5ka:
         "records_per_page": 20
     }
 
-
-    def __init__(self, start_url: str, result_path: Path):
+    def __init__(self, start_url: str, categories_url: str, result_path: Path):
         self.start_url = start_url
         self.result_path = result_path
+        self.categories_url = categories_url
 
     def _get_response(self, url, *args, **kwargs) -> requests.Response:
         while True:
@@ -39,29 +39,37 @@ class Parse5ka:
             time.sleep(1)
 
     def run(self):
-        for product in self._parse(self.start_url):
-            self._save(product)
+         for categories in self._parse_categories(self.categories_url):
+            self._save_categories(categories)       
 
-    def _parse(self, url):
+    def _parse_categories(self, url):
+        response = self._get_response(url)
+        data = response.json()
+        for categories in data:
+            yield categories
+
+    def _parse(self, url, params):         
         while url:
-            response = self._get_response(url, params=self.params)
+            response = self._get_response(url, params)
             data = response.json()
             url = data.get('next')
             for product in data.get('results', []):
-                yield product
+                yield product    
 
-    def _save(self, data):
-        fale_path = self.result_path.joinpath(f'{data["id"]}.json')
+    def _save_categories(self, data):
+        fale_path = self.result_path.joinpath(f'{data["parent_group_name"]}.json')
+        self.params["categories"] = data["parent_group_code"]
+        array_product = []
+        for product in self._parse(self.start_url, self.params):
+            array_product.append(product)
+
+        data["products"] = array_product              
+        
         fale_path.write_text(json.dumps(data, ensure_ascii=False), 'UTF-8')
 
 if __name__ == '__main__':
     file_path = Path(__file__).parent.joinpath("продукты по категориям") # "products"
     if not file_path.exists():
         file_path.mkdir()
-    parser = Parse5ka("https://5ka.ru/api/v2/special_offers/", file_path)
+    parser = Parse5ka("https://5ka.ru/api/v2/special_offers/", "https://5ka.ru/api/v2/categories/", file_path)
     parser.run()
-
-# парсим категории по адресу "https://5ka.ru/api/v2/categories/"
-# по категории вытягиваем список товаров и id категории
-# помещаем в файл json (пример: "наименование категории товара".json)
-# помещаем файлы в папку "продукты по категориям"
